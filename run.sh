3890d149920f49887ea4493224616699f2ac8197
GFan <gaojifan1991@gmail.com>
2023-01-22 09:57:20 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,42 +1,43 @@
-python model_1_predict_dev.py \
+python run_model_1.py \
   --path_train_add='/ext_info/added_fts_tr.npy'\
   --path_dev_add='/ext_info/added_fts_dev.npy'\
   --path_test_add='/ext_info/added_fts_test.npy'\
   --path_train_metamap='/ext_info/ner_metamap_train_v2.pickle'\
   --path_dev_metamap='/ext_info/ner_metamap_dev_v2.pickle'\
   --path_test_metamap='/ext_info/ner_metamap_test.pickle'
-python model_2_predict_dev.py \
+python run_model_2.py \
   --path_train_add='/ext_info/added_fts_tr.npy'\
   --path_dev_add='/ext_info/added_fts_dev.npy'\
   --path_test_add='/ext_info/added_fts_test.npy'\
   --path_train_metamap='/ext_info/ner_metamap_train_v2.pickle'\
   --path_dev_metamap='/ext_info/ner_metamap_dev_v2.pickle'\
   --path_test_metamap='/ext_info/ner_metamap_test.pickle'
-python model_3_predict_dev.py  \
+python run_model_3.py  \
   --path_train_add='/ext_info/added_fts_tr.npy'\
   --path_dev_add='/ext_info/added_fts_dev.npy'\
   --path_test_add='/ext_info/added_fts_test.npy'\
   --path_train_metamap='/ext_info/ner_metamap_train_v2.pickle'\
   --path_dev_metamap='/ext_info/ner_metamap_dev_v2.pickle'\
   --path_test_metamap='/ext_info/ner_metamap_test.pickle'
-python model_4_predict_dev.py  \
+python run_model_4.py  \
   --path_train_add='/ext_info/added_fts_tr.npy'\
   --path_dev_add='/ext_info/added_fts_dev.npy'\
   --path_test_add='/ext_info/added_fts_test.npy'\
   --path_train_metamap='/ext_info/ner_metamap_train_v2.pickle'\
   --path_dev_metamap='/ext_info/ner_metamap_dev_v2.pickle'\
   --path_test_metamap='/ext_info/ner_metamap_test.pickle'
-python model_5_predict_dev.py  \
+python run_model_5.py  \
   --path_train_add='/ext_info/added_fts_tr.npy'\
   --path_dev_add='/ext_info/added_fts_dev.npy'\
   --path_test_add='/ext_info/added_fts_test.npy'\
   --path_train_metamap='/ext_info/ner_metamap_train_v2.pickle'\
   --path_dev_metamap='/ext_info/ner_metamap_dev_v2.pickle'\
   --path_test_metamap='/ext_info/ner_metamap_test.pickle'
-python model_6_predict_dev.py  \
+python run_model_6.py  \
   --path_train_add='/ext_info/added_fts_tr.npy'\
   --path_dev_add='/ext_info/added_fts_dev.npy'\
   --path_test_add='/ext_info/added_fts_test.npy'\
   --path_train_metamap='/ext_info/ner_metamap_train_v2.pickle'\
   --path_dev_metamap='/ext_info/ner_metamap_dev_v2.pickle'\
   --path_test_metamap='/ext_info/ner_metamap_test.pickle'
+python ensemble.py
