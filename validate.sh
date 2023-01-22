b5b078ccf022b52ad936950716730de068d061e4
GFan <gaojifan1991@gmail.com>
2023-01-22 09:44:38 -0600

    Delete validate.sh

--- validate.sh
+++
@@ -1,6 +0,0 @@
-python model_dev.py \
---epoch=3 \
---learning_rate=1e-5 \
---pretrained_model=/home/jifangao/N2C2_track3/downloaded_models/PubmedBERTbase-MimicBig-EntityBERT \
---local_model=True \
---mode_train=True
